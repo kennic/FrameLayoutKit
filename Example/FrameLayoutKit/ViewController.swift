0d1ae56c995e26cc2cfd50c384e3b8830f754fbb
Nam Kennic <namkennic@me.com>
2020-04-17 12:19:00 +0700

    Updated Example

--- Example/FrameLayoutKit/ViewController.swift
+++ Example/FrameLayoutKit/ViewController.swift
@@ -10,72 +10,105 @@ import UIKit
 import FrameLayoutKit
 
 class ViewController: UIViewController {
-	var frameLayout: StackFrameLayout!
+	let frameLayout = StackFrameLayout(axis: .vertical)
 	
-	func createLabel(text: String, backgroundColor: UIColor) -> UILabel {
-		let label = UILabel()
-		label.text = text
-//		label.textAlignment = .center
-		label.textColor = .white
-		label.backgroundColor = backgroundColor
-		label.numberOfLines = 0
-		label.font = UIFont.systemFont(ofSize: 16, weight: UIFont.Weight.light)
-		return label
-	}
-
     override func viewDidLoad() {
         super.viewDidLoad()
 		
-		self.view.backgroundColor = .lightGray
+		view.backgroundColor = .lightGray
 		
-		let imageView1 = UIImageView(image: #imageLiteral(resourceName: "earth_48x48"))
-		let imageView2 = UIImageView(image: #imageLiteral(resourceName: "earth_48x48"))
+		#if targetEnvironment(macCatalyst)
+		for _ in 0..<3 {
+			let cardView = CardView()
+			view.addSubview(cardView)
+			frameLayout.append(view: cardView)
+		}
+		#else
+		let cardView = CardView()
+		view.addSubview(cardView)
+		frameLayout.append(view: cardView)
+		#endif
 		
-		let label1 = createLabel(text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua", backgroundColor: .red)
-		let label2 = createLabel(text: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat", backgroundColor: .green)
-		let label3 = createLabel(text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua", backgroundColor: .blue)
-		let label4 = createLabel(text: "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua", backgroundColor: .black)
-		let label5 = createLabel(text: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum", backgroundColor: .purple)
-		
-//		let loremText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
-//		label3.text = loremText
-		
-		let label4_5 = StackFrameLayout(axis: .vertical, distribution: .top, views: [label4, imageView2, label5])
-		label4_5.frameLayout(at: 1)?.contentAlignment = (.center, .center)
-		label4_5.spacing = 5
-		
-		let labels3_4_5 = DoubleFrameLayout(axis: .horizontal, distribution: .equal, views: [label3, label4_5])
-		labels3_4_5.spacing = 5
-		
-		frameLayout = StackFrameLayout(axis: .vertical, distribution: .top) // views: [label1, label2, imageView, labels3_4_5]
-		frameLayout.append(view: label1)
-		frameLayout.append(view: label2)
-		frameLayout.append(view: imageView1).contentAlignment = (.center, .center)
-		frameLayout.appendSpace(size: 20).debugColor = .yellow
-		frameLayout.append(frameLayout: labels3_4_5)
-		frameLayout.edgeInsets = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
-		frameLayout.showFrameDebug = true
-		frameLayout.isIntrinsicSizeEnabled = true
-		frameLayout.spacing = 5
-		
-		view.addSubview(label1)
-		view.addSubview(label2)
-		view.addSubview(label3)
-		view.addSubview(label4)
-		view.addSubview(label5)
-		view.addSubview(imageView1)
-		view.addSubview(imageView2)
+		frameLayout.spacing = 20
+		frameLayout.edgeInsets = UIEdgeInsets(top: 50, left: 50, bottom: 50, right: 50)
+		frameLayout.distribution = .center
 		view.addSubview(frameLayout)
     }
 	
 	override func viewDidLayoutSubviews() {
 		super.viewDidLayoutSubviews()
-		
-		let viewSize = view.bounds.size
-		let fitSize = CGSize(width: viewSize.width * 0.8, height: viewSize.height * 0.8)
-		let contentSize = frameLayout.sizeThatFits(fitSize)
-		frameLayout.frame = CGRect(x: (viewSize.width - contentSize.width)/2, y: (viewSize.height - contentSize.height)/2, width: contentSize.width, height: contentSize.height)
+		frameLayout.frame = view.bounds
 	}
 
 }
 
+class CardView: UIView {
+	let earthImageView = UIImageView(image: UIImage(named: "earth_48x48"))
+	let rocketImageView = UIImageView(image: UIImage(named: "rocket_32x32"))
+	let nameLabel = UILabel()
+	let dateLabel = UILabel()
+	let messageLabel = UILabel()
+	let frameLayout = StackFrameLayout(axis: .horizontal)
+	
+	init() {
+		super.init(frame: .zero)
+		
+		layer.backgroundColor = UIColor.white.cgColor
+		layer.shadowColor = UIColor.black.withAlphaComponent(0.5).cgColor
+		layer.shadowOffset = .zero
+		layer.shadowRadius = 5
+		layer.shadowOpacity = 0.6
+		layer.masksToBounds = false
+		
+		nameLabel.font = .systemFont(ofSize: 18, weight: .bold)
+		nameLabel.text = "FrameLayoutKit"
+		
+		dateLabel.font = .systemFont(ofSize: 15, weight: .thin)
+		dateLabel.text = "Example"
+		
+		messageLabel.font = .systemFont(ofSize: 18, weight: .regular)
+		messageLabel.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+		
+		[nameLabel, dateLabel, messageLabel].forEach { (label) in
+			label.numberOfLines = 0
+			label.textColor = .black
+		}
+		
+		[earthImageView, rocketImageView, nameLabel, dateLabel, messageLabel].forEach { (view) in
+			addSubview(view)
+		}
+		
+		let imageLayout = StackFrameLayout(axis: .vertical)
+		imageLayout.append(view: earthImageView).contentAlignment = (.top, .center)
+		imageLayout.appendSpace(size: 10).isFlexible = true
+		imageLayout.append(view: rocketImageView).contentAlignment = (.center, .center)
+		
+		let contentLayout = StackFrameLayout(axis: .vertical, distribution: .top)
+		contentLayout.append(view: nameLabel)
+		contentLayout.append(view: dateLabel)
+		contentLayout.appendSpace(size: 10.0)
+		contentLayout.append(view: messageLabel)
+		contentLayout.spacing = 5.0
+		
+		frameLayout.append(frameLayout: imageLayout)
+		frameLayout.append(frameLayout: contentLayout)
+		frameLayout.spacing = 15.0
+		frameLayout.edgeInsets = UIEdgeInsets(top: 15, left: 15, bottom: 15, right: 15)
+		frameLayout.showFrameDebug = true
+		addSubview(frameLayout)
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	override func sizeThatFits(_ size: CGSize) -> CGSize {
+		return frameLayout.sizeThatFits(size)
+	}
+	
+	override func layoutSubviews() {
+		super.layoutSubviews()
+		frameLayout.frame = bounds
+	}
+	
+}
